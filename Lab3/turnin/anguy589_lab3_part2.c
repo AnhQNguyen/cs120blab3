a672cf21fd6569d7d2a7ae8c905b5ac87c21beb4
Anh Nguyen <anguy589@wch136-22.cs.ucr.edu>
2021-04-11 13:39:46 -0700

    Part2

---
+++ Lab3/turnin/anguy589_lab3_part2.c
@@ -0,0 +1,55 @@
+/*	Author: anguy589
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #3  Exercise #2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.*/
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+
+  //initialize ports
+  DDRA = 0x00; PORTA = 0x0FF;
+  DDRC = 0x00; PORTC = 0x00;
+
+  //temporary variables
+  unsigned char tmpA = 0x00;
+  unsigned char tmpC = 0x00;
+
+
+  while(1) {
+    tmpA = PINA;
+      if(tmpA >= 0x01 && tmpA <= 0x02) {
+          tmpC = 0x60; //C5 and C6
+      }
+      else if(tmpA >= 0x03 && tmpA <= 0x04) {
+        tmpC = 0x70; //C5C4 and C6
+
+      }
+      else if(tmpA >= 0x05 && tmpA <= 0x06) {
+        tmpC = 0x38; //C5C4C3
+      }
+      else if(tmpA >= 0x07 && tmpA <= 0x09) {
+        tmpC = 0x3C; //C5C4C3C2
+      }
+      else if(tmpA >= 0x0A && tmpA <= 0x0C) {
+        tmpC = 0x3E; //C5C4C3C2C1
+      }
+      else if(tmpA >= 0x0D && tmpA <= 0x0F) {
+        tmpC = 0x3F; //C5C4C3C2C1C0
+      }
+
+
+      PORTC = tmpC;
+
+  }
+
+  return 1;
+
+}
